c2d6286f3afcf355910132518aedafe0d508443d
Darko Kukovec <darko@kukovec.eu>
2017-04-16 16:29:25 +0200

    Export JsonApi interfaces

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,6 +1,7 @@
+import * as JsonApi from './interfaces/JsonApi';
+export { JsonApi };
 export { Store } from './Store';
 export { Record } from './Record';
 export * from './NetworkUtils';
 export { default as IDictionary } from './interfaces/IDictionary';
-export * from './interfaces/JsonApi';
 export { IModel } from 'mobx-collection-store';
--- dist/index.js
+++ dist/index.js
@@ -3,6 +3,8 @@ function __export(m) {
     for (var p in m) if (!exports.hasOwnProperty(p)) exports[p] = m[p];
 }
 Object.defineProperty(exports, "__esModule", { value: true });
+var JsonApi = require("./interfaces/JsonApi");
+exports.JsonApi = JsonApi;
 var Store_1 = require("./Store");
 exports.Store = Store_1.Store;
 var Record_1 = require("./Record");
