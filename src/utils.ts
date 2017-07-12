937c4cc774f72dd3fd9a8f4e008d9860cf0f97c3
Andrei Zvonimir Crnkovic <andrei@infinum.co>
2017-07-12 12:24:50 +0200

    feat(functional endpoint): add util function for getting value

--- src/utils.ts
+++ src/utils.ts
@@ -100,3 +100,20 @@ export function assign(target: object, ...args: Array<object>) {
   });
   return target;
 }
+
+/**
+ * Returns the value if it's not a function. If it's a function
+ * it calls it.
+ *
+ * @export
+ * @template T
+ * @param {(T|(() => T))} target can be  anything or function
+ * @returns {T} value
+ */
+export function getValue<T>(target: T|(() => T)): T {
+  if (typeof target === 'function') {
+    return target();
+  }
+
+  return target;
+}
